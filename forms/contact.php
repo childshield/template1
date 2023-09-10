7e529953f82e8a38f8443bcfcfec0abeedf44094
Sonal <sonalaneel@gmail.com>
2023-09-10 12:04:28 -0400

    Banner updated

--- forms/contact.php
+++ forms/contact.php
@@ -4,10 +4,10 @@
     $from = $_POST['email']; // this is the sender's Email address
     $name = $_POST['name'];
     $subject = $_POST['subject'];
-    $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];    
+    $message = $name . " wrote the following on AI4Food Web:" . "\n\n" . $_POST['message'];    
     $headers = "From:" . $from;
     mail($to,$subject,$message,$headers);
-    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    //echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+    //header('Location: ../index.html#contact'); //to redirect to another page.
 
 ?>
